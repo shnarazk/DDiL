c0450c97a14f209609cd4355562b16f241befd73
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-02-24 17:12:18 +0900

    WIP: BDD

--- BDD/Basic.lean
+++ BDD/Basic.lean
@@ -1,1 +1,40 @@
-def hello := "world"
+import Std
+import Common.DDClasses
+
+inductive BDD where
+  | leaf (id' : Nat) (value : Bool)
+  | node (id' : Nat) (left right : BDD)
+deriving BEq, Hashable, Repr
+
+instance : Inhabited BDD where
+  default := .leaf 0 false
+
+def BDD.id (self : BDD) : Nat := match self with
+  | .leaf id' _ => id'
+  | .node id' _ _ => id'
+
+def BDD.low (self : BDD) : Option BDD := match self with
+  | .leaf _ _ => none
+  | .node _ left _ => some left
+
+def BDD.high (self : BDD) : Option BDD := match self with
+  | .leaf _ _ => none
+  | .node _ _ right => some right
+
+instance : DecisionDiagramTree BDD where
+  id := BDD.id
+--  newConstant : Bool → BDD := sorry
+--  addNewVar (index: Nat) (low high : BDD) : BDD := sorry
+--  isConstant : BDD → Bool := sorry
+--  unifiedKey : BDD → Nat := sorry
+--  index : BDD → Option Nat := sorry
+  low := BDD.low
+  high := BDD.high
+--  buildIndexer : BDD → Std.HashMap BDD Nat × Std.HashMap Nat BDD := sorry
+
+example : (default : BDD).id = 0 := rfl
+
+instance : ToString BDD where
+  toString := fun bdd => match bdd with
+    | .leaf id' value => s!"leaf {id'} {value}"
+    | .node id' left right => s!"node {id'} {left.id} {right.id}"
--- Common/DDClasses.lean
+++ Common/DDClasses.lean
@@ -1,15 +1,16 @@
 import Std.Data.HashMap
 
 class DecisionDiagramTree (α : Type) [BEq α] [Hashable α] where
-  len : α → Nat
-  newConstant : Bool → α
-  addNewVar (index: Nat) (low high : α) : α
-  isConstant : α → Bool
-  unifiedKey : α → Nat
-  index : α → Option Nat
+  id (self : α) : Nat
+--  len : α → Nat
+--  newConstant : Bool → α
+--  addNewVar (index: Nat) (low high : α) : α
+--  isConstant : α → Bool
+--  unifiedKey : α → Nat
+--  index : α → Option Nat
   low : α → Option α
   high : α → Option α
-  buildIndexer : α → Std.HashMap α Nat × Std.HashMap Nat α
+--  buildIndexer : α → Std.HashMap α Nat × Std.HashMap Nat α
 
 class ReducibleDecisionDiagramTree (α : Type) [BEq α] [Hashable α] extends DecisionDiagramTree α where
   reduce : α → α
