ea1acbfcf3087e857ba05b4a23de392a5e9a1f58
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-02-26 08:28:29 +0900

    WIP BDD

--- BDD/Basic.lean
+++ BDD/Basic.lean
@@ -1,40 +1,21 @@
 import Std
 import Common.DDClasses
 
-inductive BDD where
-  | leaf (id' : Nat) (value : Bool)
-  | node (id' : Nat) (left right : BDD)
-deriving BEq, Hashable, Repr
+structure BDD where
+  tree : Node
+  unusedId : Nat := 2
+deriving BEq
 
-instance : Inhabited BDD where
-  default := .leaf 0 false
+def BDD.addNewConstant (self: BDD) (_value : Bool) : BDD :=
+  self
 
-def BDD.id (self : BDD) : Nat := match self with
-  | .leaf id' _ => id'
-  | .node id' _ _ => id'
+def BDD.addNewVar (self: BDD) (_index : Nat) (_low _high : Node) : BDD :=
+  self
 
-def BDD.low (self : BDD) : Option BDD := match self with
-  | .leaf _ _ => none
-  | .node _ left _ => some left
-
-def BDD.high (self : BDD) : Option BDD := match self with
-  | .leaf _ _ => none
-  | .node _ _ right => some right
-
-instance : DecisionDiagramTree BDD where
-  id := BDD.id
---  newConstant : Bool → BDD := sorry
---  addNewVar (index: Nat) (low high : BDD) : BDD := sorry
---  isConstant : BDD → Bool := sorry
---  unifiedKey : BDD → Nat := sorry
---  index : BDD → Option Nat := sorry
-  low := BDD.low
-  high := BDD.high
+instance : DecisionDiagram BDD where
+  allNodes (self : BDD) := Node.toHashSet self.tree
+  addNewConstant := BDD.addNewConstant
+  addNewVar (self : BDD) (index : Nat) (low high : Node) := self.addNewVar index low high
 --  buildIndexer : BDD → Std.HashMap BDD Nat × Std.HashMap Nat BDD := sorry
 
-example : (default : BDD).id = 0 := rfl
-
-instance : ToString BDD where
-  toString := fun bdd => match bdd with
-    | .leaf id' value => s!"leaf {id'} {value}"
-    | .node id' left right => s!"node {id'} {left.id} {right.id}"
+example : (default : Node).index = 0 := rfl
--- Common/DDClasses.lean
+++ Common/DDClasses.lean
@@ -1,22 +1,90 @@
 import Std.Data.HashMap
+import Std.Data.HashSet
 
-class DecisionDiagramTree (α : Type) [BEq α] [Hashable α] where
-  id (self : α) : Nat
---  len : α → Nat
---  newConstant : Bool → α
---  addNewVar (index: Nat) (low high : α) : α
---  isConstant : α → Bool
---  unifiedKey : α → Nat
+open Std
+
+/--
+Implementation of node linking to two siblings in binary graph
+It holds a boolean value and a unique identifier.
+It is a derivation of `BEq`, `Hashable`, `Repr`, and `DecidableEq`.
+-/
+inductive Node where
+  | asFalse
+  | asTrue
+  | node (id : Nat) (low high : Node)
+deriving BEq, Hashable, Repr
+
+instance : Inhabited Node where
+  default := .asFalse
+
+def Node.index (self : Node) : Nat := match self with
+  | .asFalse => 0
+  | .asTrue => 1
+  | .node id _ _ => id
+
+instance : ToString Node where
+  toString (n : Node) : String := match n with
+    | .asFalse => "leaf false"
+    | .asTrue => "leaf true"
+    | .node id low high => s!"node {id} {low.index} {high.index}"
+
+def Node.newConstant (value : Bool) : Node := match value with
+  | true => .asTrue
+  | false => .asFalse
+
+def Node.newVar (index : Nat) (low high : Node) : Node :=
+  .node index low high
+
+def Node.isConstant (self : Node) : Option Bool := match self with
+  | .asFalse => some false
+  | .asTrue => some true
+  | .node _ _ _ => none
+
+def Node.toHashSet (self : Node) (set : Std.HashSet Node := Std.HashSet.empty): Std.HashSet Node := match self with
+  | .asFalse | .asTrue => set.insert self
+  | .node _ low high => set.insert self |> low.toHashSet |> high.toHashSet
+
+def Node.satisfiable (self : Node) : Bool := match self with
+  | .asFalse => false
+  | .asTrue => true
+  | .node _ low high => match low.satisfiable with
+    | true => true
+    | false => high.satisfiable
+
+/--
+Checks if the node satisfies all conditions.
+Tree traversing approach isn't efficient because it visits subtrees many times.
+-/
+def linearCount (counter : Std.HashMap Node Nat) (n : Node) : Std.HashMap Node Nat × Nat :=
+  if let some count := counter[n]? then (counter, count)
+  else
+    match n with
+    | .asFalse => (counter, 0)
+    | .asTrue => (counter, 1)
+    | .node _ low high =>
+        let (c₁, k₁) := linearCount counter low
+        let (c₂, k₂) := linearCount c₁ high
+        (c₂.insert n (k₁ + k₂), k₁ + k₂)
+
+/--
+Returns the number of satisfying assignments for the given node.
+This is the number of paths.
+-/
+def Node.numSatisfies (self : Node) : Nat :=
+  linearCount Std.HashMap.empty self |>.snd
+
+/--
+Interface for decision diagram built from `Node`.
+-/
+class DecisionDiagram (α : Type) [BEq α] where
+  --  numberOfPaths : α → Nat
+  allNodes : α → HashSet Node
+  addNewConstant : α → Bool → α
+  addNewVar (self : α) (index: Nat) (low high : Node) : α
 --  index : α → Option Nat
-  low : α → Option α
-  high : α → Option α
 --  buildIndexer : α → Std.HashMap α Nat × Std.HashMap Nat α
 
-class ReducibleDecisionDiagramTree (α : Type) [BEq α] [Hashable α] extends DecisionDiagramTree α where
+class ReducibleDecisionDiagram (α : Type) [BEq α] extends DecisionDiagram α where
   reduce : α → α
   apply : α → (Bool → Bool → Bool) → Bool → α
   compose : α → α → α
-
-class DecisionDiagram (α : Type) where
-  of (β : Type) [BEq β] [Hashable β] [DecisionDiagramTree β] : β → α
-  unwrap (β : Type) [BEq β] [Hashable β] [DecisionDiagramTree β] : α → β
