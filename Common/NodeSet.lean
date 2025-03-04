a7ff40704fa04ad974c00dcfc1353f3f0377d825
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-03-04 09:50:24 +0900

    WIP

--- Common/NodeSet.lean
+++ Common/NodeSet.lean
@@ -1,35 +1,55 @@
 import Std.Data.HashMap
+import Std.Data.HashMap.Lemmas
 import Common.Node
 
 open Std
 
+theorem zero_gt_eq_NeZero (n : Nat) (h : 0 < n) : NeZero n := by
+  have : n ≠ 0 := by omega
+  exact { out := this }
+
+@[simp]
+theorem base_hash_size_eq_two : (HashMap.ofList [(0, Node.isFalse), (1, Node.isTrue)]).size = 2 := by
+  apply HashMap.size_ofList
+  simp
+
 /-- A set for non-compact indexed nodes
-Flow: Array Node → NodeSet → Graph (= NodeSet // compact index family)
--/
+Flow: Array Node → NodeSet → Graph (= NodeSet // compact index family) -/
 structure NodeSet extends HashMap Nat Node where
   filled : NeZero toHashMap.size
 
 instance : Inhabited NodeSet where
   default :=
-    let nodes : HashMap Nat Node := HashMap.empty.insertMany [(0, Node.isFalse), (1, Node.isTrue)]
-    have : nodes.size = 2 := by sorry
-    have : nodes.size ≠ 0 := by sorry
-    have : NeZero nodes.size := by sorry -- isorry
-    { toHashMap := nodes, filled := this  }
+    let nodes : HashMap Nat Node := HashMap.ofList [(0, Node.isFalse), (1, Node.isTrue)]
+    have : nodes.size = 2 := by exact base_hash_size_eq_two
+    have : nodes.size ≠ 0 := by omega
+    have : NeZero nodes.size := by exact {out := this}
+    {toHashMap := nodes, filled := this}
 
 #check (default : NodeSet)
 #eval! (default : NodeSet).toHashMap
 
-namespace NodeSet
+def NodeSet.ofTreeNode' (tree : TreeNode)
+    (map : HashMap Nat Node := HashMap.empty) : HashMap Nat Node :=
+  match tree with
+      | TreeNode.isFalse => map.insert 0 .isFalse
+      | TreeNode.isTrue  => map.insert 0 .isTrue
+      | TreeNode.node varId low high id =>
+        let map₁ := NodeSet.ofTreeNode' low map
+        let map₂ := NodeSet.ofTreeNode' high map₁
+        map₂.insert id (.node varId low.index high.index)
 
-def ofHashMap (nodes : HashMap Nat Node) : NodeSet :=
+def NodeSet.ofHashMap (nodes : HashMap Nat Node) : NodeSet :=
   if h : 0 < nodes.size then
-      have : NeZero nodes.size := by sorry
-      { toHashMap := nodes, filled := this }
-    else
-      default
+    have : NeZero nodes.size := by sorry
+    {toHashMap := nodes, filled := this}
+  else
+    default
 
-def compact (self : NodeSet) : NodeSet :=
+def NodeSet.ofTreeNode (tree : TreeNode) : NodeSet :=
+  NodeSet.ofHashMap <| NodeSet.ofTreeNode' tree
+
+def NodeSet.compact (self : NodeSet) : NodeSet :=
   let ids : HashMap Nat Nat := HashMap.ofList <| self.keys.zipIdx
   self.toList
     |>.map
@@ -41,5 +61,3 @@ def compact (self : NodeSet) : NodeSet :=
     |> NodeSet.ofHashMap
 
 #eval! (default : NodeSet).compact
-
-end NodeSet
--- Common/Parser.lean
+++ Common/Parser.lean
@@ -1,4 +1,3 @@
--- import Batteries
 import Std.Internal.Parsec
 
 namespace ParserLib
