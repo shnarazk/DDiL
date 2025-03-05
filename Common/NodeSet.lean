57053eb8d9e0af5492c07b680193a0f09005987b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-03-05 12:29:20 +0900

    Proof work

--- Common/NodeSet.lean
+++ Common/NodeSet.lean
@@ -4,21 +4,47 @@ import Common.Node
 
 open Std
 
+section Proofs
+
+def base : HashMap Nat Node := HashMap.ofList [(0, Node.isFalse), (1, Node.isTrue)]
+
+theorem base_hash_has_zero : 0 ∈ base := by
+  rw [base]
+  simp
+
+theorem base_hash_has_one : 1 ∈ base := by
+  rw [base]
+  simp
+
 theorem zero_gt_eq_NeZero (n : Nat) (h : 0 < n) : NeZero n := by
   have : n ≠ 0 := by omega
   exact { out := this }
 
 @[simp]
-theorem base_hash_size_eq_two : (HashMap.ofList [(0, Node.isFalse), (1, Node.isTrue)]).size = 2 := by
+theorem base_hash_size_eq_two : base.size = 2 := by
   apply HashMap.size_ofList
   simp
+
+theorem base_hash_is_bounded : ∀ k ∈ base, k < base.size := by
+  simp
+  sorry
+
+theorem base_hash_has_no_hole : ∀ k < base.size, k ∈ base := by
+  simp
+  intro n h
+  have : n = 0 ∨ n = 1 := by omega
+  rcases this with h0 | h1
+  {rw [h0] ; exact base_hash_has_zero}
+  {rw [h1] ; exact base_hash_has_one}
+
+end Proofs
 
 /-- A HashMap with nifty properties
 Note: It might be non-compact.
 Flow: Array Node → NodeSet → Graph (= NodeSet // compact index family) -/
 structure NodeSet extends HashMap Nat Node where
   filled : NeZero toHashMap.size
-  bounded : ∀k ∈ toHashMap.keys, k < toHashMap.size
+  bounded : ∀k ∈ toHashMap, k < toHashMap.size
   no_hole : ∀k < toHashMap.size, k ∈ toHashMap
 
 instance : Inhabited NodeSet where
@@ -48,15 +74,11 @@ def NodeSet.ofTreeNode' (tree : TreeNode)
         map₂.insert id (.node varId low.index high.index)
 
 def NodeSet.ofHashMap (nodes : HashMap Nat Node) : NodeSet :=
-  if h : 0 < nodes.size
-      ∧ (∀k ∈ nodes.keys, k < nodes.size)
-      ∧ (∀ k < nodes.size, k ∈ nodes)
+  if h : 0 < nodes.size ∧ (∀k ∈ nodes.keys, k < nodes.size) ∧ (∀ k < nodes.size, k ∈ nodes)
   then
-    have : NeZero nodes.size := by exact zero_gt_eq_NeZero nodes.size h.left
-    { toHashMap := nodes
-      filled := this
-      bounded := h.right.left
-      no_hole := h.right.right }
+    have : NeZero nodes.size := by
+      exact zero_gt_eq_NeZero nodes.size h.left
+    {toHashMap := nodes, filled := this, bounded := h.right.left, no_hole := h.right.right}
   else
     default
 
