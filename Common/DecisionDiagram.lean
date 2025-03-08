0aa60a5bd342d500bd8a24127bf42792d3677306
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-03-08 11:18:07 +0900

    Revise Decision Diagram Interface

--- Common/DecisionDiagram.lean
+++ Common/DecisionDiagram.lean
@@ -10,12 +10,10 @@ Interface for decision diagram built from `Node`.
 Note: since diagrams are not trees, traversing them is not efficient.
 So we need some caching mechanism in data structures representing diagrams. -/
 class DecisionDiagram (α : Type) [BEq α] [GraphShape α] where
-  -- numberOfPaths : α → Nat
-  allNodes : α → HashSet (Node γ)
-  addNewConstant : α → Bool → (Node γ) × α
-  addNewNode (self : α) (varId low high : Nat) : Nat × α
+  numberOfPaths : α → Nat
+  allNodes : α → HashSet (Nat × Node)
 
-class ReducibleDecisionDiagram (α : Type) [BEq α] extends DecisionDiagram α where
+class ReducibleDecisionDiagram (α : Type) [BEq α] [GraphShape α] [DecisionDiagram α] where
   /-- Normalize to a valid representation -/
   reduce : α → α
   apply : α → (Bool → Bool → Bool) → Bool → α
